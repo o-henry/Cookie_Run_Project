fb20192d9d95008d14d3b225d13724b26dddd310
Channing <cksgod16@gmail.com>
2020-03-13 10:49:55 +0900

    Update storybook config for typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,20 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };

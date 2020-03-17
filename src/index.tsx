ef5441eabf19dc65365d9d898a30985bed23fd55
Channing <cksgod16@gmail.com>
2020-03-17 23:44:07 +0900

    Fin

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_GQL_API_SERVER,
+    // process.env.REACT_APP_GQL_API_SERVER,
+    uri: "https://web-codingtest-api.devscookie.com/graphql",
     // GQL 에러 발생시 에러 체크
     onError: ({ networkError, graphQLErrors }): void => {
         console.log('graphQLErrors', graphQLErrors)
